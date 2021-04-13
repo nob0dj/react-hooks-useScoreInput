ecd7ccdb9e8a49c8f45a480369f90d2763e7388c
nob0dj <35418827+nob0dj@users.noreply.github.com>
2021-04-13 18:55:39 +0900

    useScoreInput 완성

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
 import "./styles.css";
-import { useState } from "react";
-import useInput from "./useInput";
-import useScoreInput from "./useScoreInput";
+import useScoreInput, { useTotal, scoresValidator } from "./useScoreInput";
 
 export default function App() {
-  //10글자미만인지 유효성 검사
-  const name = useInput("Mr.", (value) => value.length <= 10);
-  // const score = useScoreInput({
-  //   maxValue: 100
-  // });
-
-  const [total, setTotal] = useState(0);
-  /**
-   * 사용자 입력값이 아직 useState반영전이므로,
-   * 매개인자 score객체의 newValue를 total계산에 반영한다.
-   *
-   * @param {Array.fo(useScoreInput)} scores
-   * @param {useScoreInput} score
-   */
-  const calcTotal = (scores, score) => {
-    // console.log(scores); // [{"id":"practice00","value":1},{"id":"practice01","value":0}]
-    // console.log("score@calTotal = ", score); // {id: "practice00", value: 3}
-    const computed = scores.reduce((accumulator, current) => {
-      // console.log("current@calTotal", current); // {id: "practice00", value: 12, onInput: ƒ} {id: "practice01", value: 0, onInput: ƒ}
-      const { id, value } = current;
-      if (id === score.id) {
-        //현재 입력중인 input태그인 경우, 인자의 새 입력값을 더해준다.
-        return accumulator + score.value;
-      }
-      return accumulator + value;
-    }, 0);
-    setTotal(computed); // async처리되므로, 아래 total은 변경전 값이다.
-    // console.log(`computed = ${computed}`);
-    // console.log(`total = ${total}`);
-  };
+  const { total, calcTotal } = useTotal(0);
 
   let scores = [];
   scores.push(
     useScoreInput({
       id: "practice00",
-      maxValue: 40,
+      maxValue: 30,
       callback: calcTotal,
       parent: scores //부모배열 참조
     })
@@ -47,32 +16,40 @@ export default function App() {
   scores.push(
     useScoreInput({
       id: "practice01",
-      maxValue: 60,
+      maxValue: 30,
+      callback: calcTotal,
+      parent: scores //부모배열 참조
+    })
+  );
+  scores.push(
+    useScoreInput({
+      id: "practice02",
+      maxValue: 40,
       callback: calcTotal,
       parent: scores //부모배열 참조
     })
   );
 
+  const { isOk, maxValues, sum } = scoresValidator(scores);
+  // console.log(isOk, maxValues, sum);
+  if (!isOk) {
+    throw new Error(`총점이 100점이 아닙니다. 총점(${sum}) - ${maxValues}`);
+  }
+
   return (
     <div className="App">
-      <h1>Hello useInput</h1>
-      {/* <input
-        placeholder="Name"
-        value={name.value}
-        onChange={name.onChange}
-      /> */}
-      <div>
-        <input placeholder="Name" {...name} />
-      </div>
       <h1>Hello useScoreInput</h1>
       <p>점수 input태그별 maxValue 유효성 검사 및 총점을 실시간 계산함.</p>
       <div>
-        {/* <input placeholder="점수" {...score} /> */}
-        {scores.map((score, index) => (
-          <div key={index}>
-            <input placeholder="점수" {...score} />
-          </div>
-        ))}
+        {scores.map((score, index) => {
+          //score에서 maxValue속성 제외
+          const { maxValue, ..._score } = score;
+          return (
+            <div key={index}>
+              <input placeholder="점수" {..._score} /> / {maxValue}
+            </div>
+          );
+        })}
         <div>총점 : {total}</div>
       </div>
     </div>
--- src/useScoreInput.js
+++ src/useScoreInput.js
@@ -13,6 +13,12 @@ const useScoreInput = (score) => {
     const {
       target: { value }
     } = e;
+    console.log(value);
+    //소수점 있는 경우
+    if (value.slice(value.length - 1) === ".") {
+      setValue(value);
+      return;
+    }
     if (value <= maxValue) {
       // useState setter는 비동기처리 된다.
       //callback실행시 value가 반영이 안되어 있으므로, 새 value를 포한된 객체를 매개인자로 전달.
@@ -21,7 +27,48 @@ const useScoreInput = (score) => {
       callback(parent, newScore);
     }
   };
-  return { id, value, onInput };
+  return { id, value, maxValue, onInput };
+};
+
+export const useTotal = () => {
+  const [total, setTotal] = useState(0);
+  /**
+   * 사용자 입력값이 아직 useState반영전이므로,
+   * 매개인자 score객체의 newValue를 total계산에 반영한다.
+   *
+   * @param {Array.fo(useScoreInput)} scores
+   * @param {useScoreInput} score
+   */
+  const calcTotal = (scores, score) => {
+    // console.log(scores); // [{"id":"practice00","value":1},{"id":"practice01","value":0}]
+    // console.log("score@calTotal = ", score); // {id: "practice00", value: 3}
+    const computed = scores.reduce((accumulator, current) => {
+      // console.log("current@calTotal", current); // {id: "practice00", value: 12, onInput: ƒ} {id: "practice01", value: 0, onInput: ƒ}
+      const { id, value } = current;
+      if (id === score.id) {
+        //현재 입력중인 input태그인 경우, 인자의 새 입력값을 더해준다.
+        return accumulator + score.value;
+      }
+      return accumulator + value;
+    }, 0);
+    setTotal(computed); // async처리되므로, 아래 total은 변경전 값이다.
+    // console.log(`computed = ${computed}`);
+    // console.log(`total = ${total}`);
+  };
+
+  return { total, calcTotal };
+};
+
+export const scoresValidator = (scores) => {
+  let isOk = true;
+  let sum = 0;
+  let maxValues = [];
+  scores.forEach(({ maxValue }) => {
+    sum += maxValue;
+    maxValues.push(maxValue);
+  });
+  if (sum !== 100) isOk = false;
+  return { isOk, maxValues, sum };
 };
 
 export default useScoreInput;
